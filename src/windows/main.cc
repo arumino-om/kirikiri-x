b575419418abc8cb44dfe31542bc07f4d2233c62
Arumino <102341161+arumino-om@users.noreply.github.com>
2023-07-07 23:13:54 +0900

    #2 を解決

--- src/windows/main.cc
+++ src/windows/main.cc
@@ -1,7 +1,14 @@
 #include "filesystem_impl.h"
+#include "SDL.h"
 #include "libruntime.h"
+#include <iostream>
+#include <windows.h>
+#include <codecvt>
 
-int main() {
+int main(int argv, char** args) {
+    std::ios_base::sync_with_stdio(false);
+    std::wcout.imbue(std::locale{std::locale{},new std::codecvt_utf8_utf16<wchar_t>{}});
+
     LibRuntime::KrkrRuntime::filesystem = new WindowsFileSystem();
     LibRuntime::KrkrRuntime::start_runtime();
 }
