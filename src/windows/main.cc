183bfd1aeedac64447cc164ba4671a2c159e322c
Arumino <102341161+arumino-om@users.noreply.github.com>
2023-08-23 16:09:20 +0900

    コンソールへのアタッチを改善

--- src/windows/main.cc
+++ src/windows/main.cc
@@ -7,23 +7,18 @@
 #include <fcntl.h>
 
 void alloc_console() {
-    AllocConsole();
-
-    HANDLE out_handle = GetStdHandle(STD_OUTPUT_HANDLE);
-    int out_console_handle = _open_osfhandle(reinterpret_cast<intptr_t>(out_handle), _O_TEXT);
-    *stdout = *_fdopen(out_console_handle, "w");
-    setvbuf(stdout, nullptr, _IONBF, 0);
-
-    HANDLE err_handle = GetStdHandle(STD_ERROR_HANDLE);
-    int err_console_handle = _open_osfhandle(reinterpret_cast<intptr_t>(err_handle), _O_TEXT);
-    *stderr = *_fdopen(err_console_handle, "w");
-    setvbuf(stderr, nullptr, _IONBF, 0);
+    if (!AttachConsole(ATTACH_PARENT_PROCESS)) AllocConsole();
+    freopen("CON", "r", stdin);
+    freopen("CON", "w", stdout);
+    freopen("CON", "w", stderr);
 }
 
 int main(int argv, char** args) {
 #if DEBUG
     alloc_console();
 #endif
+    setlocale(LC_ALL,"");
+
     LibRuntime::KrkrRuntime::filesystem = new WindowsFileSystem();
     LibRuntime::KrkrRuntime::console = new WindowsConsole();
     LibRuntime::KrkrRuntime::start_runtime();
