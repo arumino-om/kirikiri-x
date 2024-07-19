4eaedf7d1bbea407813e1ec2344c7e01901b7089
Arumino <git@arumino-om.net>
2024-07-19 11:33:54 +0900

    refactor: 関数並び替え

--- src/libruntime/native_objects/system.cc
+++ src/libruntime/native_objects/system.cc
@@ -9,21 +9,35 @@ tjs_uint32 SystemNativeClass::ClassID = -1;
 SystemNativeClass::SystemNativeClass() : tTJSNativeClass(TJS_W("System")) {
     TJS_BEGIN_NATIVE_MEMBERS(System)
         TJS_DECL_EMPTY_FINALIZE_METHOD
-        // constructor
+
+        //===== CONSTRUCTOR =====
         TJS_BEGIN_NATIVE_CONSTRUCTOR_DECL_NO_INSTANCE(System)
             return TJS_S_OK;
         TJS_END_NATIVE_CONSTRUCTOR_DECL(System)
 
-        // methods
-        TJS_BEGIN_NATIVE_METHOD_DECL(exit)
-            exit(param[0]->AsInteger());
-            return TJS_S_OK;    //関数が戻ることはないはず
-        TJS_END_NATIVE_METHOD_DECL(exit)
 
-        TJS_BEGIN_NATIVE_METHOD_DECL(terminate)
-            KrkrRuntime::request_quit(param[0]->AsInteger());
+        //===== METHODS =====
+        TJS_BEGIN_NATIVE_METHOD_DECL(addContinuousHandler)
+            if (numparams < 1) return TJS_E_BADPARAMCOUNT;
+            auto clo = param[0]->AsObjectClosureNoAddRef();
+            EventManager::add_continuous_handler(clo);
             return TJS_S_OK;
-        TJS_END_NATIVE_METHOD_DECL(terminate)
+        TJS_END_NATIVE_METHOD_DECL(addContinuousHandler)
+
+
+        TJS_BEGIN_NATIVE_METHOD_DECL(assignMessage)
+            if (numparams < 2) return TJS_E_BADPARAMCOUNT;
+            ScriptManager::assign_message(param[0]->GetString(), param[1]->GetString());
+        TJS_END_NATIVE_METHOD_DECL(assignMessage)
+
+
+        TJS_BEGIN_NATIVE_METHOD_DECL(createAppLock)
+            if (numparams < 1) return TJS_E_BADPARAMCOUNT;
+            auto func_result=  KrkrRuntime::sysfunc->create_app_lock(param[0]->GetString());
+            if (result != nullptr) *result = func_result;
+            return TJS_S_OK;
+        TJS_END_NATIVE_METHOD_DECL(createAppLock)
+
 
         TJS_BEGIN_NATIVE_METHOD_DECL(getArgument)
             if (numparams < 1) return TJS_E_BADPARAMCOUNT;
@@ -38,23 +52,19 @@ SystemNativeClass::SystemNativeClass() : tTJSNativeClass(TJS_W("System")) {
             return TJS_S_OK;
         TJS_END_NATIVE_METHOD_DECL(getArgument)
 
+
+        TJS_BEGIN_NATIVE_METHOD_DECL(exit)
+            exit(param[0]->AsInteger());
+            return TJS_S_OK;    //関数が戻ることはないはず
+        TJS_END_NATIVE_METHOD_DECL(exit)
+
+
         TJS_BEGIN_NATIVE_METHOD_DECL(setArgument)
             if (numparams < 2) return TJS_E_BADPARAMCOUNT;
             KrkrRuntime::set_argument(param[0]->GetString(), param[1]->GetString());
             return TJS_S_OK;
         TJS_END_NATIVE_METHOD_DECL(setArgument)
 
-        TJS_BEGIN_NATIVE_METHOD_DECL(addContinuousHandler)
-            if (numparams < 1) return TJS_E_BADPARAMCOUNT;
-            auto clo = param[0]->AsObjectClosureNoAddRef();
-            EventManager::add_continuous_handler(clo);
-            return TJS_S_OK;
-        TJS_END_NATIVE_METHOD_DECL(addContinuousHandler)
-
-        TJS_BEGIN_NATIVE_METHOD_DECL(assignMessage)
-            if (numparams < 2) return TJS_E_BADPARAMCOUNT;
-            ScriptManager::assign_message(param[0]->GetString(), param[1]->GetString());
-        TJS_END_NATIVE_METHOD_DECL(assignMessage)
 
         TJS_BEGIN_NATIVE_METHOD_DECL(shellExecute)
             if (numparams < 1) return TJS_E_BADPARAMCOUNT;
@@ -63,12 +73,11 @@ SystemNativeClass::SystemNativeClass() : tTJSNativeClass(TJS_W("System")) {
             KrkrRuntime::sysfunc->execute(program, args);
         TJS_END_NATIVE_METHOD_DECL(shellExecute)
 
-        TJS_BEGIN_NATIVE_METHOD_DECL(createAppLock)
-            if (numparams < 1) return TJS_E_BADPARAMCOUNT;
-            auto func_result=  KrkrRuntime::sysfunc->create_app_lock(param[0]->GetString());
-            if (result != nullptr) *result = func_result;
-            return TJS_S_OK;
-        TJS_END_NATIVE_METHOD_DECL(createAppLock)
+
+        TJS_BEGIN_NATIVE_METHOD_DECL(terminate)
+                KrkrRuntime::request_quit(param[0]->AsInteger());
+                return TJS_S_OK;
+        TJS_END_NATIVE_METHOD_DECL(terminate)
     TJS_END_NATIVE_MEMBERS
 }
 
