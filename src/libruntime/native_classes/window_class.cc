7d8b232fa69903c20b275ecf87d439f5172218f2
Arumino <git@arumino-om.net>
2024-07-31 11:58:50 +0900

    feat: 完全なadd関数の定義を追加

--- src/libruntime/native_classes/window_class.cc
+++ src/libruntime/native_classes/window_class.cc
@@ -18,8 +18,12 @@ WindowNativeClass::WindowNativeClass() : tTJSNativeClass(TJS_W("Window")) {
 
         TJS_BEGIN_NATIVE_METHOD_DECL(add) {
             TJS_GET_NATIVE_INSTANCE(_this, NativeInstances::WindowNativeInstance)
-            _this->
+            if (numparams < 1) return TJS_E_BADPARAMCOUNT;
+            auto clo = param[0]->AsObjectClosureNoAddRef();
+            _this->add_object(clo);
+            return TJS_S_OK;
         }
+        TJS_END_NATIVE_METHOD_DECL(add)
     }
     TJS_END_NATIVE_MEMBERS
 }
