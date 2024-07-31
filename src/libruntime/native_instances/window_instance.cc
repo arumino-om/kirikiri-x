821483454493c682661d9ea5bbce662891c54600
Arumino <git@arumino-om.net>
2024-07-31 17:11:55 +0900

    feat: 簡易的なウィンドウ関係の処理を実装

--- src/libruntime/native_instances/window_instance.cc
+++ src/libruntime/native_instances/window_instance.cc
@@ -1,4 +1,5 @@
 #include "window_instance.h"
+#include "../window_manager.h"
 
 using namespace LibRuntime::NativeInstances;
 
@@ -8,12 +9,40 @@ WindowNativeInstance::WindowNativeInstance() {
 
 tjs_error TJS_INTF_METHOD WindowNativeInstance::Construct(tjs_int numparams, tTJSVariant **param, iTJSDispatch2 *tjs_obj) {
     this->window = SDL_CreateWindow(TJS_N("Window"), SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 300, 300, 0);
+    this->renderer = SDL_CreateRenderer(this->window, -1, SDL_RENDERER_ACCELERATED);
+    WindowManager::register_window(this);
     return TJS_S_OK;
 }
 
-void WindowNativeInstance::add_object(tTJSVariantClousre clo) {
+void TJS_INTF_METHOD WindowNativeInstance::Invalidate() {
+    for (auto &obj : objects) {
+        obj.Release();
+    }
+    objects.clear();
+
+    if (window != nullptr) {
+        SDL_DestroyWindow(window);
+        window = nullptr;
+    }
+
+    WindowManager::unregister_window(this);
+}
+
+void WindowNativeInstance::add_object(tTJSVariantClosure clo) {
     if (objects.end() != std::find(objects.begin(), objects.end(), clo)) return;
 
     objects.push_back(clo);
     clo.AddRef();
+}
+
+void WindowNativeInstance::remove_object(tTJSVariantClosure clo) {
+    auto it = std::find(objects.begin(), objects.end(), clo);
+    if (it == objects.end()) return;
+
+    clo.Release();
+    objects.erase(it);
+}
+
+void WindowNativeInstance::update() {
+    
 }
--- src/libruntime/native_instances/window_instance.h
+++ src/libruntime/native_instances/window_instance.h
@@ -1,3 +1,4 @@
+#pragma once
 #include <SDL.h>
 #include <tjs.h>
 #include <tjsError.h>
@@ -7,12 +8,15 @@ namespace LibRuntime::NativeInstances {
     class WindowNativeInstance : public tTJSNativeInstance {
     public:
         WindowNativeInstance();
-        tjs_error TJS_INTF_METHOD Construct(tjs_int numparams, tTJSVariant **param, iTJSDispatch2 *tjs_obj);
-        // void TJS_INTF_METHOD Invalidate();
+        tjs_error TJS_INTF_METHOD Construct(tjs_int numparams, tTJSVariant **param, iTJSDispatch2 *tjs_obj) override;
+        void TJS_INTF_METHOD Invalidate() override;
 
         void add_object(tTJSVariantClosure clo);
+        void remove_object(tTJSVariantClosure clo);
+        void update();
     private:
         SDL_Window *window;
+        SDL_Renderer *renderer;
 
         std::vector<tTJSVariantClosure> objects;
     };
