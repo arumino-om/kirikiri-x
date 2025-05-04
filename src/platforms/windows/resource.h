f078fda4d2e1af0c0a03e7e77d76a030a81a3b3c
Arumino <git@arumino-om.net>
2025-05-04 16:35:24 +0900

    add: コミット漏れ

---
+++ src/platforms/windows/resource.h
@@ -0,0 +1,19 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ で生成されたインクルード ファイル。
+// krkrx.rc で使用
+//
+#define IDD_KRKRX_ABOUT                 101
+#define IDCOPYENV                       1000
+#define IDC_EDIT1                       1001
+#define IDC_ABOUT_CONTENT               1001
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        103
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1002
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
